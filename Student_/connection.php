cf135e5f9e9b37cb71fdcffcb24b754ec9d67755
anand-12345 <anandshirole17@gmail.com>
2021-03-24 08:42:14 +0530

    student

---
+++ Student_/connection.php
@@ -0,0 +1,6 @@
+<?php
+
+	$db=mysqli_connect("localhost:3307","root","","library");  
+					/* server name, username, passwor, database name */
+
+?>
---
+++ Student_/index.php
@@ -0,0 +1,87 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Online Library Management System
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style type="text/css">
+	nav
+	{
+		float: right;
+		word-spacing: 30px;
+		padding: 20px;
+	}
+	nav li 
+	{
+		display: inline-block;
+		line-height: 80px;
+	}
+</style>
+</head>
+
+
+<body>
+	<div class="wrapper">
+		<header>
+		<div class="logo">
+			<img src="images/5.jpg">
+			<h1 style="color: white;"> LIBRARY MANAGEMENT SYSTEM</h1>
+		</div>
+
+		<?php
+		if(isset($_SESSION['login_user']))
+		{
+			?>
+				<nav>
+					<ul>
+						<li><a href="index.php">HOME</a></li>
+						<li><a href="books.php">BOOKS</a></li>
+						<li><a href="logout.php">LOGOUT</a></li>
+						<li><a href="feedback.php">FEEDBACK</a></li>
+					</ul>
+				</nav>
+			<?php
+		}
+		else
+		{
+			?>
+						<nav>
+							<ul>
+								<li><a href="index.php">HOME</a></li>
+								<li><a href="books.php">BOOKS</a></li>
+								<li><a href="student_login.php">LOGIN</a></li>
+								<li><a href="registration.php">SIGN-UP</a></li>
+								<li><a href="feedback.php">FEEDBACK</a></li>
+							</ul>
+						</nav>
+		<?php
+		}
+			
+		?>
+		</header>
+		<section>
+		<div class="sec_img">
+			<br><br><br>
+			<div class="box">
+				<br><br><br><br>
+				<h1 style="text-align: center; font-size: 35px; font-family:times new roman;">Welcome to library</h1><br><br>
+				<h1 style="text-align: center;font-size: 25px; font-family:times new roman;">("Libraries store the energy that fuels the imagination. They open up windows to the world and inspire us to explore and achieve, and contribute to improving our quality of life.") - Sidney Sheldon</h1><br>
+			</div>
+		</div>
+		</section>
+		
+
+	</div>
+	<?php  
+
+		include "footer.php";
+	?>
+</body>
+</html>
